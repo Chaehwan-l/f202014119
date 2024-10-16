d91d106ec4b2cbfac1b24e96b8fe8a3b6be951cc
Lim, Chaehwan <legoch16@gmail.com>
2024-10-16 09:17:51 +0900

    five

--- src/f202014119/World.java
+++ src/f202014119/World.java
@@ -3,6 +3,10 @@ public class World {
     public static void run() {
       System.out.println("202014119");
     }
+
+    public static void run2() {
+        System.out.println("hello world");
+    }
   
   }
   
